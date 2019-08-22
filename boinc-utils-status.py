5bee98a8af57f8dabb32e33e04574e788897f8e2
Yevhen Shyshkan <eugene.tx200@gmail.com>
2019-08-22 21:40:42 +0300

    Minimal version using socket module

--- boinc-utils-status.py
+++ boinc-utils-status.py
@@ -1,12 +1,16 @@
-from xmlrpc.client import ServerProxy, Error, ProtocolError
+#from xmlrpc.client import ServerProxy, Error, ProtocolError
+import socket
 
-proxy = ServerProxy("http://127.0.0.1:31416")
-try:
-    proxy.system.listMethods()
-except ProtocolError as err:
-    print("A protocol error occurred")
-    print("Error code: %d" % err.errcode)
 
+HOST = 'localhost'
+PORT = 31416
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'<boinc_gui_rpc_request><get_host_info/></boinc_gui_rpc_request>\003')
+    data = s.recv(2048)
 
 if __name__ == "__main__":
     print("Boinc utils status")
+    print('Received', repr(data))
+    
