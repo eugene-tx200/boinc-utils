9c5b56abc0c145dd5e13a235b8f7952ba10e341b
Yevhen Shyshkan <eugene.tx200@gmail.com>
2019-08-22 22:13:11 +0300

    new file:   utils.py

--- boinc-utils-status.py
+++ boinc-utils-status.py
@@ -1,16 +1,15 @@
 #from xmlrpc.client import ServerProxy, Error, ProtocolError
 import socket
-
+from utils import request
 
 HOST = 'localhost'
 PORT = 31416
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'<boinc_gui_rpc_request><get_host_info/></boinc_gui_rpc_request>\003')
+    s.sendall(request('<get_host_info/>'))
     data = s.recv(2048)
 
 if __name__ == "__main__":
     print("Boinc utils status")
     print('Received', repr(data))
-    
