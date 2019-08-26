acf24a356fd6f8dd0e57b637a18817874f3cda77
Yevhen Shyshkan <eugene.tx200@gmail.com>
2019-08-26 17:58:04 +0300

    Optimized names

--- boinc-utils-status.py
+++ boinc-utils-status.py
@@ -1,13 +1,13 @@
 #from xmlrpc.client import ServerProxy, Error, ProtocolError
 import socket
-from utils import request
+from utils import request_template
 
 HOST = 'localhost'
 PORT = 31416
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(request('<get_host_info/>'))
+    s.sendall(request_template('<get_host_info/>'))
     data = s.recv(2048)
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,3 +1,3 @@
-def request(req):
+def request_template(req):
     s = '<boinc_gui_rpc_request>{}</boinc_gui_rpc_request>\003'.format(req)
     return bytes(s, 'utf-8')
