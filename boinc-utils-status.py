3dd73a95582d491116cbb593b8834b0f64183b56
Yevhen Shyshkan <eugene.tx200@gmail.com>
2019-08-28 14:24:31 +0300

    Better print()

--- boinc-utils-status.py
+++ boinc-utils-status.py
@@ -1,18 +1,21 @@
-#from xmlrpc.client import ServerProxy, Error, ProtocolError
 import socket
 from request import Request
-
 import xml.etree.ElementTree as ET
 
 
 HOST = 'localhost'
 PORT = 31416
 
+def print_child(data):
+    for child in data:
+        print ('{}: {}'.format(child.tag, child.text))
+        if child:
+            print_child(child)
+
 if __name__ == "__main__":
     print("Boinc utils status")
     r = Request(HOST, PORT)
     r.connect()
     data = r.get_host_info()
-    # ugly example
-    print(ET.tostring(data).decode())
+    print_child(data)
     r.close()
