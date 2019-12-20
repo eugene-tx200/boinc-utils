28a956de6f95c02c38b5715293d8d9966aab2f73
Yevhen Shyshkan <eugene.tx200@gmail.com>
2019-12-20 18:42:17 +0200

    Merge boinc_utils.pt to cmd.py

--- cmd.py
+++ cmd.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 import argparse
-from boinc_utils import (get_host_info,
-                         exchange_versions,
-                         get_state,
-                         acct_mgr_info)
+import socket
+import xml.etree.ElementTree as ET
+from request import Request
+
+
+HOST = 'localhost'
+PORT = 31416
+# Password stored in /etc/boinc-client/gui_rpc_auth.cfg
+PASSWORD = 'G3d2H5z1M7'
+
+def print_child(data):
+    for child in data:
+        print ('{}: {}'.format(child.tag, child.text))
+        if child:
+            print_child(child)
 
 
 parser = argparse.ArgumentParser()
@@ -19,13 +30,31 @@ parser.add_argument("--acct_mgr_info", help="show current account manager info",
 args = parser.parse_args()
 
 if args.get_host_info:
-    get_host_info()
+    r = Request(HOST, PORT)
+# Using 'connect()' and 'close()' methods
+#    r.connect()
+#    data = r.get_host_info()
+#    print_child(data)
+#    r.close()
+# Using 'with'statement
+    with r:
+        data = r.get_host_info()
+        print_child(data)
 
 if args.client_version:
-    exchange_versions()
+    r = Request(HOST, PORT)
+    with r:
+        data = r.exchange_versions()
+        print_child(data)
 
 if args.get_state:
-    get_state()
+    r = Request(HOST, PORT)
+    with r:
+        data = r.get_state()
+        print_child(data)
 
 if args.acct_mgr_info:
-    acct_mgr_info()
+    r = Request(HOST, PORT, PASSWORD)
+    with r:
+        data = r.acct_mgr_info()
+        print_child(data)
